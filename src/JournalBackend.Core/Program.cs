bcea6772015ef56fba18706a0c86797666e245d5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-11-22 18:40:48 -0500

    feat(db): integrate db context and add swagger docs

--- src/JournalBackend.Core/Program.cs
+++ src/JournalBackend.Core/Program.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel;
+using JournalBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<JournalDbContext>(options => options.UseInMemoryDatabase("items"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Journal API",
+        Description = "Backend API for a journaling web application.",
+        Version = "v1",
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +26,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Journal API v1");
+    });
 }
 
 app.UseHttpsRedirection();
