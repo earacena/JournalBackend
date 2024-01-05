65347fe8fbf6ef9726d92970cefb6a61f2cb082e
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-01-04 21:22:20 -0500

    feat: add authorization to swagger docs

--- src/JournalBackend.Core/Program.cs
+++ src/JournalBackend.Core/Program.cs
@@ -20,6 +20,26 @@ builder.Services.AddSwaggerGen(c =>
         Version = "v1",
     });
 
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                }
+            },
+            new string[] {}
+        }
+    });
 });
 
 string? JwtKey = Environment.GetEnvironmentVariable("DOTNET_JWT_KEY");
